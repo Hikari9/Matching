de2b514ea4309d93b0f1c9777b92c48dc7470723
Hikari9 <thericotiongson@gmail.com>
2015-04-21 14:30:33 +0800

    Optimized likelihood function.

--- likelihood.py
+++ likelihood.py
@@ -4,88 +4,106 @@
 import numpy as np
 import pandas as pd
 
-# initializes a zero matrix data frame based on row and column labels.
+# initializes a likelihood data frame based on row and column labels.
+# Performs a content-based filtering technique using features.
 # vectorizer can be 'tfidf' or 'count'
-def likelihood_matrix(row_labels, column_labels, vectorizer='tfidf', ngram_range=(2,4)):
+def likelihood_matrix(row_labels, column_labels, vectorizer='tfidf', ngram_range=(2,3)):
 
 	rows = len(row_labels)
-	columns = len(column_labels)
-	matrix = np.zeros((rows, columns))
+	columns = len(column_labels)	
 	
-	data_frame = pd.DataFrame(matrix, index=row_labels, columns=column_labels)
-	
-	data_frame.row_labels = row_labels
-	data_frame.column_labels = column_labels
-	
-	# add a 'features' attribute to data frame
+	# start vectorizing
 	from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 	
-	if vectorizer == 'tfidf':
-		vectorizer = TfidfVectorizer
-	elif vectorizer == 'count':
-		vectorizer = CountVectorizer
-	else:
-		return data_frame # don't add features
+	if vectorizer == 'tfidf': vectorizer = TfidfVectorizer
+	elif vectorizer == 'count': vectorizer = CountVectorizer
+	else: raise 'Vectorizer should be tfidf or count'
 	
 	vectorizer = vectorizer(analyzer='char', ngram_range=ngram_range)
 	
-	df = data_frame
-	df.unique_labels = list(set(df).union(set(df.index)))
-	df.features = vectorizer.fit_transform(df.unique_labels).toarray()
-	df.cache = {label: vector for label, vector in zip(df.unique_labels, df.features)}
-	df.vectorizer = vectorizer
+	# extract vectors
+	unique_labels = list(set(column_labels).union(set(row_labels)))
+	features = vectorizer.fit_transform(unique_labels).toarray()
 	
-	return df
+	# cache a mapping of label to corresponding vector
+	vector_cache = {label: vector for label, vector in zip(unique_labels, features)}
 	
-
-
-
-# returns similarity function for the cosine similarity
-# between vectorized labels in a data frame
-def default_similarity(data_frame):
-
-	df = data_frame
+	# get all pairwise cosine similarity
+	# this will be the initial content of the matrix
 	
-	def get_vector(text):
-		if text not in df.cache:
-			df.cache[text] = df.vectorizer.transform(text).toarray()
-		return df.cache[text]
-		
+	row_vectors = [vector_cache[label] for label in row_labels]
+	column_vectors = [vector_cache[label] for label in column_labels]
 	
 	from sklearn.metrics.pairwise import cosine_similarity
 	
-	def calculate_similarity(text1, text2):
-		v1 = get_vector(text1)
-		v2 = get_vector(text2)
-		return cosine_similarity(v1, v2)[0][0]
+	matrix = cosine_similarity(row_vectors, column_vectors)
 	
-	return calculate_similarity
+	# create pandas data frame object
+	data_frame = pd.DataFrame(matrix, index=row_labels, columns=column_labels)
+	
+	# append attributes
+	
+	data_frame.row_labels = row_labels
+	data_frame.column_labels = column_labels
+	data_frame.unique_labels = unique_labels
+	data_frame.row_vectors = row_vectors
+	data_frame.column_vectors = column_vectors
+	data_frame.features = features
+	data_frame.vector_cache = vector_cache
+	data_frame.vectorizer = vectorizer
+	data_frame.row_similarity_cache = {}
+	data_frame.column_similarity_cache = {}
+	
+	return data_frame
 
 
-# computes delta likelihood using the following function:
-#	clabels = df
-#	rlabels = df.index
-#	vr = [similarity(ur, label) for label in rlabels]
-#	vc = [similarity(vr, label) for label in clabels]
-# 	dL = vr * vc^T
-def delta_likelihood(data_frame, user_row, user_column, similarity = None):
-	
-	if similarity == None:
-		similarity = default_similarity(data_frame)
+# computes delta likelihood using the following steps: (non-verbatim)
+# clabels = df.column_labels
+# rlabels = df.row_labels
+# vr = [cos_similarity(ur, label) for label in rlabels]
+# vc = [cos_similarity(vr, label) for label in clabels]
+# dL = vr * vc^T
+
+def delta_likelihood(data_frame, user_row, user_column):
 	
 	df = data_frame
-	row_labels = df.row_labels
-	column_labels = df.column_labels
+	row_vectors = df.row_vectors
+	column_vectors = df.column_vectors
 	
-	ur = user_row
-	uc = user_column
+	def get_vector(text):
+		if text in df.vector_cache: 
+			vector = df.vector_cache[text]
+		else:
+			vector = df.vectorizer.transform([text]).toarray()[0]
+			df.vector_cache[text] = vector
+		return vector
+		
+	# get cosine similarities from user data to corresponding labels
+	from sklearn.metrics.pairwise import cosine_similarity
 	
-	vr = [similarity(ur, label) for label in row_labels]
-	vc = [similarity(uc, label) for label in column_labels]
+	# check first if exists in cache for performance
+	if user_row in df.row_similarity_cache:
+		row_similarities = df.row_similarity_cache[user_row]
+	else:
+		user_row_vector = get_vector(user_row)
+		# untransposed vector matrix
+		row_similarities = cosine_similarity(row_vectors, [user_row_vector])
+		# add back to cache
+		df.row_similarity_cache[user_row] = row_similarities
 	
-	vr = np.matrix([[x] for x in vr]) # transposed
-	vcT = np.matrix(vc) # untransposed
+	# do the same for columns
+	if user_column in df.column_similarity_cache:
+		column_similarities = df.column_similarity_cache[user_column]
+	else:
+		user_column_vector = get_vector(user_column)
+		# transposed vector matrix
+		column_similarities = cosine_similarity([user_column_vector], column_vectors)
+		# add back to cache
+		df.column_similarity_cache[user_column] = column_similarities
 	
-	return pd.DataFrame((vr * vcT).A, index=row_labels, columns=column_labels)
+	
+	# combine computations by multiplying
+	matrix = np.matrix(row_similarities) * np.matrix(column_similarities)
+	return pd.DataFrame(matrix, index=df.row_labels, columns=df.column_labels)
 	
 	
